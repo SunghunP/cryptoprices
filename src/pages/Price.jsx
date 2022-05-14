a73ccf5faa0af86a42589c63e6de494c8aef78a0
Joe Park <sunghun.josephp@gmail.com>
2022-05-14 12:56:17 -0700

    Display coin price

--- src/pages/Price.jsx
+++ src/pages/Price.jsx
@@ -3,19 +3,30 @@ import { useParams } from "react-router-dom";
 
 export default function Price() {
   const apiKey = "pk_f638507918de416286887219f2aa4cbd";
-  const { symbol } = useParmas();
+  const { symbol } = useParams();
   const url = `https://cloud.iexapis.com/stable/crypto/${symbol}/price?token=${apiKey}`;
 
   const [coin, setCoin] = useState(null);
 
-  const getCoin = async () => {
+  async function getCoin() {
     const data = await fetch(url).then((res) => res.json());
     setCoin(data);
-  };
+  }
 
   useEffect(() => {
     getCoin();
   }, []);
 
-  return <h1>This is the price Component</h1>;
+  const loaded = () => {
+    return (
+      <div>
+        <h1>{symbol}</h1>
+        <h2>{coin.price}</h2>
+      </div>
+    );
+  };
+
+  const loading = () => <h1>Loading...</h1>;
+
+  return coin ? loaded() : loading();
 }
